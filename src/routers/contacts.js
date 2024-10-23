020c987f2ed9c206fcd27a7ff9506a62cd9d55fc
Sergey Shulga <shul.sergio@gmail.com>
2024-10-23 03:07:42 +0300

    v 3.2 add post, patch, del

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import {
+  createContactController,
+  deleteContactController,
+  getAllContactsController,
+  getContactByIdController,
+  patchContactController,
+} from '../controllers/contacts.js';
 
 const router = Router();
 
-router.get('/places', ctrlWrapper(getAllContacts));
-router.get('/places/:placeId', ctrlWrapper(getContactById));
+router.get('/contacts', ctrlWrapper(getAllContactsController));
+router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.post('/contacts', ctrlWrapper(createContactController));
+router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
