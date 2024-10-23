2b55943d4d8290d49cdf2ed6d78eeed03efda82f
Sergey Shulga <shul.sergio@gmail.com>
2024-10-23 23:17:54 +0300

    v 3.26 fix middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,14 @@
+import createHttpError from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
+  if (err instanceof createHttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    return;
+  }
   res.status(500).json({
     message: 'Something went wrong',
     error: err.message,
